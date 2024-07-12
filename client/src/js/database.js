15b1b6913c214583d6d0d3cceae77b3f1436f21e
Fabricio Guacuto <guacutofabricio@gmail.com>
2024-07-11 22:46:09 -0700

    Implemented asset caching, configured Workbox plugins for service worker and manifest file, and added CSS loaders and Babel to Webpack

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,39 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+    console.log('Adding content to the database');
+    
+    const jate_db = await openDB('jate', 1);
+
+    const tx = jate_db.transaction('jate', 'readwrite');
+
+    const store = tx.objectStore('jate');
+
+    const request = store.add(content);
+
+    const result = await request;
+
+    console.log(`New content added ${result}`);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+    console.log('Getting the information from the database');
+
+    const jate_db = await openDB('jate', 1);
+
+    const tx = jate_db.transaction('jate', 'readonly');
+
+    const store = tx.objectStore('jate');
+
+    const request =  store.getAll();
+
+    const result = await request;
+
+    console.log(`Results ${result}`);
+
+    return result;
+}
 
 initdb();
